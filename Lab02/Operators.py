d9e7cf4fe1b493f25cf82d71595e879a1e0e4424
LidLow <margoshow6@gmail.com>
2025-01-23 19:49:41 +0500

    modified:   Lab02/Booleans.py 	new file:   Lab02/Dictionaries/Access Dictionary Items.py 	new file:   Lab02/Dictionaries/Add Dictionary Items.py 	new file:   Lab02/Dictionaries/Change Dictionary Items.py 	new file:   Lab02/Dictionaries/Copy Dictionaries.py 	new file:   Lab02/Dictionaries/Dictionaries.py 	new file:   Lab02/Dictionaries/Dictionary Methods.py 	new file:   Lab02/Dictionaries/Loop Dictionaries.py 	new file:   Lab02/Dictionaries/Nested Dictionaries.py 	new file:   Lab02/Dictionaries/Remove Dictionary Items.py 	new file:   Lab02/For Loops.py 	new file:   Lab02/If ... Else.py 	new file:   Lab02/Lists/Access List Items.py 	new file:   Lab02/Lists/Add List Items.py 	new file:   Lab02/Lists/Change List Items.py 	new file:   Lab02/Lists/Copy Lists.py 	new file:   Lab02/Lists/Join Lists.py 	new file:   Lab02/Lists/List Comprehension.py 	new file:   Lab02/Lists/List Methods.py 	new file:   Lab02/Lists/Lists.py 	new file:   Lab02/Lists/Loop Lists.py 	new file:   Lab02/Lists/Remove List Items.py 	new file:   Lab02/Lists/Sort Lists.py 	new file:   Lab02/Operators.py 	new file:   Lab02/Sets/Access Set Items.py 	new file:   Lab02/Sets/Add Set Items.py 	new file:   Lab02/Sets/Join Sets.py 	new file:   Lab02/Sets/Loop Sets.py 	new file:   Lab02/Sets/Remove Set Items.py 	new file:   Lab02/Sets/Set Methods.py 	new file:   Lab02/Sets/Sets.py 	new file:   Lab02/Tuples/Access Tuple Items.py 	new file:   Lab02/Tuples/Join Tuples.py 	new file:   Lab02/Tuples/Loop Tuples.py 	new file:   Lab02/Tuples/Tuple Methods.py 	new file:   Lab02/Tuples/Tuples.py 	new file:   Lab02/Tuples/Unpack Tuples.py 	new file:   Lab02/Tuples/Update Tuples.py 	new file:   Lab02/While Loops.py

---
+++ Lab02/Operators.py
@@ -0,0 +1,30 @@
+#1
+print(10 + 5)
+
+
+"""
+Python divides the operators in the following groups:
+    Arithmetic operators
+    Assignment operators
+    Comparison operators
+    Logical operators
+    Identity operators
+    Membership operators
+    Bitwise operators
+
+()	                                                Parentheses	
+**	                                                Exponentiation	
++x  -x  ~x	                                        Unary plus, unary minus, and bitwise NOT	
+*  /  //  %	                                        Multiplication, division, floor division, and modulus	
++  -	                                            Addition and subtraction	
+<<  >>	                                            Bitwise left and right shifts	
+&	                                                Bitwise AND	
+^	                                                Bitwise XOR	
+|	                                                Bitwise OR	
+==  !=  >  >=  <  <=  is  is not  in  not in 	    Comparisons, identity, and membership operators	
+not	                                                Logical NOT	
+and	                                                AND	
+or	                                                OR
+
+
+"""
