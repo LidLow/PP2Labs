84c5484aac2199e64fbbb64f4e493ac3033e0616
LidLow <margoshow6@gmail.com>
2025-03-03 12:41:50 +0500

    modified:   Lab06/Exercises/BuiltInFunc.py 	modified:   Lab06/Exercises/Dir'n'Files.py

--- Lab06/Exercises/BuiltInFunc.py
+++ Lab06/Exercises/BuiltInFunc.py
@@ -1,16 +1,13 @@
 #1. Write a Python program with builtin function to multiply all the numbers in a list
-def func_1(list):
-    sum = 1
-
-    for i in range(len(list)):
-        sum *= list[i]
+def func_1(list, sum = 1):
+    for x in range(len(list)): sum *= x
 
     return sum
 
 
 #2. Write a Python program with builtin function that accepts a string and calculate the number of upper case letters and lower case letters
 def func_2(string):
-    numLower= 0
+    numLower = 0
 
     for x in string:
         if x.lower() == x:
@@ -21,25 +18,20 @@ def func_2(string):
 
 #3. Write a Python program with builtin function that checks whether a passed string is palindrome or not.
 def func_3(string):
-    list = [x for x in string]
-    list.reverse()
-
-    return "".join(list).lower() == string.lower()
+    return "".join(list(reversed(string))).lower() == string.lower()
 
 
 #4. Write a Python program that invoke square root function after specific milliseconds.
 def func_4(number, milsec):  
-    __import__("time").sleep(milsec / 1000)     #О Великий Python Docs
+    __import__("time").sleep(milsec / 1000) 
     
     return f"Square root of {number} after {milsec} miliseconds is {pow(number, 0.5)}"
-    
+
 
 #5. Write a Python program with builtin function that returns True if all elements of the tuple are true.
 def func_5(tuple):
-    for b in tuple:
-        if not b:
-            return b
+    return all(tuple)
         
 
 #Testing section
-print(func_4(25, 5))
+print(func_3("Dood"))
--- Lab06/Exercises/Dir'n'Files.py
+++ Lab06/Exercises/Dir'n'Files.py
@@ -1,42 +1,69 @@
 import os
-from pathlib import *
 
+os.chdir(r"C:\Users\margo\OneDrive\Рабочий стол\PP2Labs\Lab06\Exercises")
 
 #1. Write a Python program to list only directories, files and all directories, files in a specified path.
-def func_1(directory):
-    return os.listdir(directory)
+def func_1(path):
+    listCurrentDir = [child.name for child in os.scandir('./') ]
+    listCustomDir = [child.name for child in os.scandir(path) ]
+
+    return listCurrentDir, listCustomDir
 
 
 #2. Write a Python program to check for access to a specified path. Test the existence, readability, writability and executability of the specified path
-def func_2(directory):
-    if Path.is_file(directory):
-        Path.open(directory)
-        Path.dir
-
+def func_2(path):
+    return ("Existance:", os.access()), ("Readability;", os.access(path, os.R_OK)), ("Writability:", os.access(path, os.W_OK)), ("Executability:", os.access(path, os.X_OK))
+    
 
 #3. Write a Python program to test whether a given path exists or not. If the path exist find the filename and directory portion of the given path.
-
+def func_3(path):
+    if os.path.exists(path):
+        return os.path.dirname(path), os.path.basename(path)
+    else:
+        return "Error"
 
 
 #4. Write a Python program to count the number of lines in a text file.
-
+def func_4(textFile):
+    with open(textFile, "r", encoding="utf-8") as file:
+        return sum(1 for line in file)
 
 
 #5. Write a Python program to write a list to a file.
+def func_5(textFile, list):
+    with open(textFile, "a") as file:
+        file.write(str(list))
 
+    return True
 
 
 #6. Write a Python program to generate 26 text files named A.txt, B.txt, and so on up to Z.txt
+from string import ascii_uppercase as auc
 
+def func_6():
+    for c in auc:
+        file = open(f"{c}.txt", "x")
+
+    return True
 
 
 #7. Write a Python program to copy the contents of a file to another file
+import shutil
 
-
+def func_7(file):
+    defaultPath = os.getcwd()
+    
+    shutil.copy2(file, defaultPath)
+    return True
 
 #8. Write a Python program to delete file by specified path. Before deleting check for access and whether a given path exists or not.
-
+def func_8(path):
+    if os.path.exists(path) and os.access(path, os.F_OK):
+        os.remove(path)
+        return True
+    else:
+        return False
 
 
 #Testing section
-print(func_2(r"C:\Users\margo\OneDrive\Рабочий стол\PP2Labs\Lab01"))
+print()
